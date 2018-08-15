0fb5cf40a338fca5f26b3a349d23a45f19a07452
lucasdpn <lucasdpnunes@gmail.com>
2018-08-15 12:31:00 -0300

    interactive and additional plots

--- src/plots.py
+++ src/plots.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from lib.skymapper import skymapper as skm
 
 
@@ -28,3 +29,32 @@ def plot_map(data):
                                 np.asarray(dec),
                                 np.asarray(signal))
     # fig.savefig(save_as + '.png')
+
+
+def plot_signal_area(data, NSIDE=4096):
+    signals = np.array(data).T[0]
+    sig_vals = sorted(list(set(signals)))
+    nsig = np.array([signals[signals >= s].size for s in sig_vals])
+
+    print(signals.size)
+
+    area_pix = 4 * np.pi * (180. / np.pi) ** 2 / (12 * 4096 ** 2)
+
+    ax1 = plt.axes()
+    ax1.plot(sig_vals, nsig)
+    ax1.set_ylabel('$N_{pix}$')
+    ax1.set_xlabel('signal>')
+
+    fig, axes = plt.subplots()
+    axes.plot(sig_vals, nsig)
+    axes.set_ylabel('$N_{pix}$')
+    axes.set_xlabel('signal>')
+
+    ax2 = axes.twinx()
+    ax2.plot(sig_vals, 100 * nsig / float(signals.size))
+    ylim = ax2.get_ylim()
+    for x in range(0, 101, 20):
+        ax2.axhline(x, ls='--', color='.5', lw=.8)
+    ax2.set_ylim(ylim)
+    ax2.set_ylabel('$Area (\%)' + '\; [%.2f\, deg^2]$' % (area_pix * signals.size))
+    fig.show()
